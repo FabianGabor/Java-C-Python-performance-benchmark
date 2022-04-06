58dcf2b961fcaebbd7628ef879c9fd5c046a948d
Fábián Gábor <info@fabiangabor.com>
2022-04-06 17:04:43 +0200

    benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#C
-echo "Compiling C with -O0 (default)"
+
+echo "C with -O0 (default)"
 gcc MatrixMultiplication.c -O1 matrix
 ./matrix
 
-echo "Compiling C with -O1"
+echo "C with -O1"
 gcc MatrixMultiplication.c -O1 matrix
 ./matrix
 
-echo "Compiling C with -O2"
+echo "C with -O2"
 gcc MatrixMultiplication.c -O2 matrix
 ./matrix
 
-echo "Compiling C with -O3"
+echo "C with -O3"
 gcc MatrixMultiplication.c -O3 matrix
 ./matrix
 
-#Java
+echo "Java"
 javac MatrixMultiplication.java
 java MatrixMultiplication
 
-#Python
+echo "Python"
 python MatrixMultiplication.py
